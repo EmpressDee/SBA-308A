8c122b8a9643aa60e6215d6ee4a6334ccc99bd57
Claudena Daniel <cjdaniel0530@gmail.com>
2026-06-11 11:59:26 -0400

    added button element and event listener

--- user.js
+++ user.js
@@ -1,1 +1,8 @@
+
 const pokeSearch = document.getElementById("searchInput");
+const button = document.getElementById("searchBtn");
+
+button.addEventListener("click", (e)=>{
+const searchValue = pokeSearch.value;
+ fetchPokemon(searchValue);
+});
