4d5516ab2dbfe47588a463c274dee489c1a44b09
Claudena Daniel <cjdaniel0530@gmail.com>
2026-06-11 15:28:33 -0400

    added image element to display when user searches pokemon

--- pokeuser.js
+++ pokeuser.js
@@ -1,4 +1,10 @@
 
-const displayPokemon = (pokemon) => {
+export const displayPokemon = (pokemon) => {
+const pokemonData = document.getElementById("pokemonResults");
+
+pokemonData.innerHTML = `<h2>${pokemon.name}</h2>`
+const image = document.createElement("img")
+image.src = `${pokemon.sprites.front_default}`;
+pokemonData.appendChild(image)
 
 }
