9cec6481e9fa1d8247c90424ce457a980e75ce0d
Claudena Daniel <cjdaniel0530@gmail.com>
2026-06-11 23:26:18 -0400

    fixed error

--- pokeuser.js
+++ pokeuser.js
@@ -9,10 +9,15 @@ export const displayPokemon = (pokemon) => {
 
 export const displayGallery = (pokemon) => {
   const gallery = document.getElementById("pokemonGallery");
+
   pokemon.map((p) => {
-    const card = document.createElement("div"));
-  gallery.innerHTML = `${pokemon.sprites.front_default}`;
-  gallery.appendChild(displayGallery);
+    const card = document.createElement("div");
+    card.innerHTML = `<img src="${p.sprites.front_default}"> <h3>${p.name}</h3>`;
+    gallery.appendChild(card);
+  
+})
 };
 ///need to fix...doing something wrong. 
 //map- create card element for each pokemon, show name and pic, append the card
+//gallery.innerHTML = `${pokemon.sprites.front_default}`;
+  //gallery.appendChild(displayGallery);
