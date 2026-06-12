2d0cedbe376e19a785f15692755da21cd35bcf85
Claudena Daniel <cjdaniel0530@gmail.com>
2026-06-11 22:16:21 -0400

    added eventlistener for pokemon list

--- mainscript.js
+++ mainscript.js
@@ -1,13 +1,19 @@
-import { fetchPokemon, PokemonList } from "./scriptapi";
+import { fetchPokemon, PokemonList } from "./scriptapi.js";
+import { displayPokemon } from "./pokeuser.js"
 
 
-
+document.addEventListener("DOMContentLoaded",async (e) => {
+    const pokemonList = await PokemonList();
+});
 
 
 const pokeSearch = document.getElementById("searchInput");
 const button = document.getElementById("searchBtn");
 
-button.addEventListener("click", (e)=>{
+button.addEventListener("click", async (e)=>{
 const searchValue = pokeSearch.value;
- fetchPokemon(searchValue);
+ const pokemon = await fetchPokemon(searchValue);
+ displayPokemon (pokemon)
+
+
 });
