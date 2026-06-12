bdc90bcd76ef4f31c9b98a55d39aad2e52df3f44
Claudena Daniel <cjdaniel0530@gmail.com>
2026-06-11 21:26:41 -0400

    added type to be displayed

--- pokeuser.js
+++ pokeuser.js
@@ -2,9 +2,11 @@
 export const displayPokemon = (pokemon) => {
 const pokemonData = document.getElementById("pokemonResults");
 
-pokemonData.innerHTML = `<h2>${pokemon.name}</h2>`
+pokemonData.innerHTML = `<h2>${pokemon.name}</h2> <p>Type: ${pokemon.types[0].type.name}</p>`;
 const image = document.createElement("img")
 image.src = `${pokemon.sprites.front_default}`;
-pokemonData.appendChild(image)
+pokemonData.appendChild(image);
+
+
 
 }
