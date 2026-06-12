4021e6cb4751b9048c923f06b4274d6a0e57d9be
Claudena Daniel <cjdaniel0530@gmail.com>
2026-06-11 23:10:31 -0400

    created card element

--- pokeuser.js
+++ pokeuser.js
@@ -1,12 +1,18 @@
-
 export const displayPokemon = (pokemon) => {
-const pokemonData = document.getElementById("pokemonResults");
+  const pokemonData = document.getElementById("pokemonResults");
 
-pokemonData.innerHTML = `<h2>${pokemon.name}</h2> <p>Type: ${pokemon.types[0].type.name}</p>`;
-const image = document.createElement("img")
-image.src = `${pokemon.sprites.front_default}`;
-pokemonData.appendChild(image);
+  pokemonData.innerHTML = `<h2>${pokemon.name}</h2> <p>Type: ${pokemon.types[0].type.name}</p>`;
+  const image = document.createElement("img");
+  image.src = `${pokemon.sprites.front_default}`;
+  pokemonData.appendChild(image);
+};
 
-
-
-}
+export const displayGallery = (pokemon) => {
+  const gallery = document.getElementById("pokemonGallery");
+  pokemon.map((p) => {
+    const card = document.createElement("div"));
+  gallery.innerHTML = `${pokemon.sprites.front_default}`;
+  gallery.appendChild(displayGallery);
+};
+///need to fix...doing something wrong. 
+//map- create card element for each pokemon, show name and pic, append the card
