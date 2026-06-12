ec6aaa810bfd483f18a49bd850c496502227ef8f
Claudena Daniel <cjdaniel0530@gmail.com>
2026-06-11 22:28:13 -0400

    updated eventlistener

--- mainscript.js
+++ mainscript.js
@@ -4,6 +4,7 @@ import { displayPokemon } from "./pokeuser.js"
 
 document.addEventListener("DOMContentLoaded",async (e) => {
     const pokemonList = await PokemonList();
+    const allPokemon = await Promise.all(pokemonList.map(p => fetchPokemon(p.name))); //map creates array of all pokemon;promise.all fires all requests at once
 });
 
 
